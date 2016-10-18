ebcab50095c74eed64a0c5ed000d8012534ba7ae
Matthieu-Lescieux <MatthieuLescieux@hotmail.fr>
2016-10-18 12:16:23 +0200

    MLE : Working app

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -36,14 +36,45 @@ class DefaultController extends Controller
 
     /**
      * @param Request $request
-     * @param $id
+     * @param $pizzaId
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     *
+     * @Route("/order/{pizzaId}", requirements={"pizzaId" = "\d+"}, name="order")
      */
-    public function orderAction(Request $request, $id)
+    public function orderAction(Request $request, $pizzaId)
+    {
+        $data = ['id' => $pizzaId];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'pizzapi.herokuapp.com/orders');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: goldenpizza'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data, JSON_NUMERIC_CHECK));
+        curl_exec($ch);
+
+        return $this->redirectToRoute('homepage');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     *
+     * @Route("/admin", name="admin")
+     */
+    public function adminAction(Request $request)
     {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'pizzapi.herokuapp.com/orders/'.$id);
+        curl_setopt($ch, CURLOPT_URL, 'pizzapi.herokuapp.com/orders');
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: goldenpizza'));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $response = curl_exec($ch);
+
+        $orders = json_decode($response);
+
+        // replace this example code with whatever you need
+        return $this->render('default/admin.html.twig', array(
+            'orders' => $orders
+        ));
     }
 }
