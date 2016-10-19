e08abfb4623185cf8baa4494c15c612778e926d5
Matthieu-Lescieux <MatthieuLescieux@hotmail.fr>
2016-10-19 17:10:21 +0200

    MLE : Dev dependencies commented

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,13 +18,13 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Circle\RestClientBundle\CircleRestClientBundle(),
             new CacheBundle\CacheBundle(),
-            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+//            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+//            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+//            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+//            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         return $bundles;
