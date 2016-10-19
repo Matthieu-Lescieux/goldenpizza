ce5fb53aeecd4c9d731fb8dc4b12904a47626377
Matthieu-Lescieux <MatthieuLescieux@hotmail.fr>
2016-10-19 17:04:27 +0200

    MLE : Load sensio generator

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Circle\RestClientBundle\CircleRestClientBundle(),
             new CacheBundle\CacheBundle(),
+            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
