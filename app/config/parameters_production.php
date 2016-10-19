aa8de1b44ce60d1f3b50f2671a94335e15071e90
Matthieu-Lescieux <MatthieuLescieux@hotmail.fr>
2016-10-19 12:09:58 +0200

    MLE : Fix

--- app/config/parameters_production.php
+++ app/config/parameters_production.php
@@ -1,12 +1,16 @@
 <?php
     $db = parse_url(getenv('CLEARDB_DATABASE_URL'));
 
-    $container->setParameter('database_driver', 'pdo_mysql');
     $container->setParameter('database_host', $db['host']);
     $container->setParameter('database_port', $db['port']);
     $container->setParameter('database_name', substr($db["path"], 1));
     $container->setParameter('database_user', $db['user']);
     $container->setParameter('database_password', $db['pass']);
+    $container->setParameter('database_host2', $db['host2']);
+    $container->setParameter('database_port2', $db['port2']);
+    $container->setParameter('database_name2', substr($db["path2"], 1));
+    $container->setParameter('database_user2', $db['user2']);
+    $container->setParameter('database_password2', $db['pass2']);
     $container->setParameter('secret', getenv('SECRET'));
     $container->setParameter('locale', 'en');
     $container->setParameter('mailer_transport', null);
